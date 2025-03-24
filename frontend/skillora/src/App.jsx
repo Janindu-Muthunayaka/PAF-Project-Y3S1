a6de7cb665babf94bacd489f5708a9d3fa2b82c2
Bumal <146937811+IT22004468@users.noreply.github.com>
2025-03-24 23:18:45 +0530

    Update App.jsx
    rename name to bumal

--- frontend/skillora/src/App.jsx
+++ frontend/skillora/src/App.jsx
@@ -25,7 +25,7 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
         <p>
-conflict recoled by ish
+conflict recoled by Bumal
         </p>
       </div>
       <p className="read-the-docs">
