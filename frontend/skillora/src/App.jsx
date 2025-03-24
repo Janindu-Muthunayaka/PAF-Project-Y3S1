30cd7a306eab5581271bb09dc158d86952f9cc02
Janindu-Muthunayaka <it22072238@my.sliit.lk>
2025-03-24 22:43:37 +0530

    Push #2

--- frontend/skillora/src/App.jsx
+++ frontend/skillora/src/App.jsx
@@ -25,7 +25,7 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
         <p>
-          Testing hehehe git push
+          Testing hehehe git push #2
         </p>
       </div>
       <p className="read-the-docs">
