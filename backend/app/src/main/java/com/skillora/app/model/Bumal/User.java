d9fe3db64c0914d9872c14ebea115f23a9572732
Ishara Madusanka <IT22259134@my.sliit.lk>
2025-03-30 10:04:11 +0530

    backednd login fix
    login backend fix

---
+++ backend/app/src/main/java/com/skillora/app/model/Bumal/User.java
@@ -0,0 +1,31 @@
+package com.skillora.app.model.Bumal;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Document(collection = "Users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String userName;
+    private String email;
+    private String password;
+    private String dob;
+    private String gender;
+    private String profilePicURL;
+    private String userType;
+    private List<String> followers = new ArrayList<>();
+    private List<String> following = new ArrayList<>();
+
+}
