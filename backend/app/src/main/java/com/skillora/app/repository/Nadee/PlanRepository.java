1126da70b6cefc34f3bc94a53a618cc4e1f585b8
VindiX28 <wgnadeedevindi@gmail.com>
2025-04-24 23:06:58 +0530

    refactor: update PlanRepository.java

--- backend/app/src/main/java/com/skillora/app/repository/Nadee/PlanRepository.java
+++ backend/app/src/main/java/com/skillora/app/repository/Nadee/PlanRepository.java
@@ -1,11 +1,17 @@
 package com.skillora.app.repository.Nadee;
 
+import java.util.Optional;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.skillora.app.model.Nadee.Plans;
 
 @Repository
 public interface PlanRepository extends MongoRepository<Plans, String> {
+
+    @Query("{'plan' : ?0}")
+    Optional<Plans> findByName(String name);
 
 }
